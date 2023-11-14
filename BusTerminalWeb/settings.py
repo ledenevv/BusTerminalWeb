801f94695abe7ebed0cb6e463c551080aae3d8bc
ledenevv <vitaliyledenevv@gmail.com>
2023-11-14 20:24:33 +0300

    Added templates folder and default index.html + urls.

--- BusTerminalWeb/settings.py
+++ BusTerminalWeb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e__ab*vho9(3t=aturulswnx$a8)o+ge^6_et*#arji53uwie%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'example.com', 'www.example.com']
 
 
 # Application definition
