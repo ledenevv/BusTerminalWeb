5c8543afa321cf16805e5af7d2543e5a79209d13
ledenevv <vitaliyledenevv@gmail.com>
2023-11-16 16:05:45 +0300

    Basic django  registration and login were developed as well as html pages were designed

--- BusTerminalWeb/urls.py
+++ BusTerminalWeb/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from main.urls import urlpatterns
 from main.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', index ),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('main.urls'))
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate
 
 def index(request):
     return render(request, 'main/index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # перенаправление на главную страницу после входа
+    else:
+        form = AuthenticationForm()
+    return render(request, 'main/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # перенаправление на страницу входа после успешной регистрации
+    else:
+        form = UserCreationForm()
+    return render(request, 'main/register.html', {'form': form})
